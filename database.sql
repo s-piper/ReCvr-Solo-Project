ee3a7fb2130308e5227d51a3731fc2f89b98e3a8
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-28 10:28:09 -0500

    Setup database and added SQL to databasefile

--- database.sql
+++ database.sql
@@ -3,8 +3,50 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+
+--tabe: prime_app
+
+CREATE TABLE "User" (
+	"Id" serial NOT NULL,
+	"Username" varchar(255) NOT NULL UNIQUE,
+	"Password" varchar(255) NOT NULL,
+	"Zipcode" varchar(255) NOT NULL,
+	CONSTRAINT "User_pk" PRIMARY KEY ("Id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "Items" (
+	"Id" serial NOT NULL,
+	"Name" varchar(255) NOT NULL,
+	"Brand" varchar(255) NOT NULL,
+	"Model" varchar(255) NOT NULL,
+	"Upload" varchar(255) NOT NULL,
+	"User_Id" int NOT NULL,
+	CONSTRAINT "Items_pk" PRIMARY KEY ("Id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "Room" (
+	"Id" serial NOT NULL,
+	"Room Name" varchar(255) NOT NULL,
+	"Description" varchar(10000) NOT NULL,
+	"Upload" varchar(255) NOT NULL,
+	"User_Id" int NOT NULL,
+	CONSTRAINT "Room_pk" PRIMARY KEY ("Id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "Insurance" (
+	"Id" serial NOT NULL,
+	"Company" varchar(255) NOT NULL,
+	"Phone" int NOT NULL,
+	"Policy Number" varchar(255) NOT NULL,
+	"Value" int NOT NULL,
+	"User_Id" varchar(255) NOT NULL,
+	CONSTRAINT "Insurance_pk" PRIMARY KEY ("Id")
+) WITH (
+  OIDS=FALSE
 );
