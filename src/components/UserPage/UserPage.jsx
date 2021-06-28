5a08139c3d5b507b2a780786126d62e85701da59
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-28 15:00:59 -0500

    setup basic design of 1.2

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,78 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './UserPage.css';
+
+
+
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+// import { Container, Typography } from '@material-ui/core';
+// import Card from 'react';
+// import CardHeader from '@material-ui/core/CardHeader'
+// import CardContent from '@material-ui/core/CardContent'
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+  // const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  const moveToDecision = () =>{
+    history.push()
+  };
+  const moveToCataloged = () =>{
+    history.push()
+  };
+  const moveToInsurance = () =>{
+    history.push()
+  };
+
+
+
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+
+    <div className="userPageSet">
+      <div className="userPageBox" onClick={moveToDecision}>
+        <h2>Add Items</h2>
+        <img src="https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" 
+          width="250px"
+          height="250px"
+          />
+        <p>Start saving your items</p>
+
+      </div>
+
+      <div className="userPageBox" onClick={moveToCataloged}>
+        <h2>Catalogued</h2>
+        <img src="https://images.pexels.com/photos/4792282/pexels-photo-4792282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+          width="250px"
+          height="250px"
+          />
+        <p>View Saved Items</p>
+
+      </div>
+
+      <div className="userPageBox" onClick={moveToInsurance}>
+        <h2>Insurance</h2>
+        <img src="https://images.pexels.com/photos/7821914/pexels-photo-7821914.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+          width="250px"
+          height="250px"
+          />
+        <p>Your Insurance</p>
+
+      </div>
+
+
+
+
+
     </div>
+    
+   
   );
 }
 
---
+++ src/components/UserPageCard/UserPageCard.jsx
@@ -0,0 +1,69 @@
+  import React from 'core-js/library/fn/reflect/es7/metadata'
+  
+  export default function UserPageCard() {
+    return (
+        <div>
+
+        </div>
+    )
+
+
+
+
+  }
+  
+  
+  //   <div class="row center-cols center-align" >
+    //     <div class="col m4">
+    //       <Card className={classes.root} onClick={goToItemsDecision}>
+    //         <CardContent>
+    //           <Typography variant="h2" >
+    //             Add Items
+    //     </Typography>
+    //           <br></br>
+    //           <Typography variant="outlined" component="p">
+    //             Get started adding your items
+    //     </Typography>
+    //         </CardContent>
+    //         {/* <CardActions>
+    //     <Button size="medium">Learn More</Button>
+    //   </CardActions> */}
+    //       </Card>
+    //     </div>
+    //   </div>
+    //   <div class="row center-cols center-align" >
+    //     <div class="col m4">
+    //       <Card className={classes.root} onClick={goToItemsDecision}>
+    //         <CardContent>
+    //           <Typography variant="h2" >
+    //             Cataloged
+    //     </Typography>
+    //           <br></br>
+    //           <Typography variant="outlined" component="p">
+    //             View what you've saved
+    //     </Typography>
+    //         </CardContent>
+    //         {/* <CardActions>
+    //     <Button size="medium">Learn More</Button>
+    //   </CardActions> */}
+    //       </Card>
+    //     </div>
+    //   </div>
+    //   <div class="row center-cols center-align" >
+    //     <div class="col m4">
+    //       <Card className={classes.root} onClick={goToItemsDecision}>
+    //         <CardContent>
+    //           <Typography variant="h2" >
+    //             Insurance
+    //     </Typography>
+    //           <br></br>
+    //           <Typography variant="outlined" component="p">
+    //             Your insurance info
+    //     </Typography>
+    //         </CardContent>
+    //         {/* <CardActions>
+    //     <Button size="medium">Learn More</Button>
+    //   </CardActions> */}
+    //       </Card>
+    //     </div>
+    //   </div>
