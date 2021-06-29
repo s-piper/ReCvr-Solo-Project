37ed87bcb05aad94cf60aa189e25a497b961416f
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-29 13:35:06 -0500

    Built out skeleton and added state for textfields

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import './AddItem.css'
 
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 
 function AddItem() {
 
+    const history=useHistory();
+    const dispatch = useDispatch();
+
+    const[itemName, setItemName]=useState('');
+    const[brand, setBrand]=useState('');
+    const[model, setModel]=useState('');
+    const[media, setMedia]=useState('');
+
+
+
     return (
-        <div>
-            <div>
+        <div className="addItemSet">
+            <div className="addItembox">
                 <h2>Individual Item</h2>
 
                 <form>
-                    <input required placeholder="Item Name*" /> 
+                    <TextField
+                        onChange={(event) => setSpecies(event.target.value)} 
+                        id="outlined-basic" 
+                        label="Item Name" 
+                        variant="outlined" 
+                        required /> 
                     <br />
-                    <input required placeholder="Brand*" />
+                    <TextField id="outlined-basic" 
+                        label="Brand" 
+                        variant="outlined" 
+                        required />
                     <br />
-                    <input placeholder="Model" />
+                    <TextField id="outlined-basic" 
+                        label="Model" 
+                        variant="outlined" />
                     <br />
-                    <input placeholder="Picture/Video URL" />
+                    <TextField id="outlined-basic" 
+                        label="Picture/Video URL" 
+                        variant="outlined" />
                     <br />
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" 
+                        color="primary">
                         Save
                     </Button>
                     <p>*Required</p>
                 </form>
             </div>
+
+            <div className="addItembox">
+                <h2>New Room</h2>
+                <img src="https://images.pexels.com/photos/1454806/pexels-photo-1454806.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                    width="350px"
+                    height="250px"
+                />
+                <p>
+                    The small stuff can add up. Pick a room and describe
+                    what's in it.
+                    </p>
+                <p>
+                    Bedroom: 9 Levis Jeans, 4 Pillows, 2 Ikea Nightstands
+                    </p>
+                <Button variant="contained" color="primary"
+                    onClick={() => { history.push('/room') }} >
+                    Add Room
+                 </Button>
+            </div>
+
 
         </div>
     )
