25a46977b0b86107164b75dd0ebf9ee06b7e0484
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-29 11:55:31 -0500

    Created input fields and button

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,8 +1,30 @@
+import React from 'react';
 
-function AddItem (){
+import Button from '@material-ui/core/Button';
+
+
+function AddItem() {
 
     return (
         <div>
+            <div>
+                <h2>Individual Item</h2>
+
+                <form>
+                    <input required placeholder="Item Name*" /> 
+                    <br />
+                    <input required placeholder="Brand*" />
+                    <br />
+                    <input placeholder="Model" />
+                    <br />
+                    <input placeholder="Picture/Video URL" />
+                    <br />
+                    <Button variant="contained" color="primary">
+                        Save
+                    </Button>
+                    <p>*Required</p>
+                </form>
+            </div>
 
         </div>
     )
