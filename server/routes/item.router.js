b85320c724b24cf88d0bb4208af5e9548dc8a725
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-29 14:59:03 -0500

    post is making it to item router. Finishing route to database

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
@@ -12,8 +15,22 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/', rejectUnauthenticated,  (req, res) => {
+  if(req.isAuthenticated()){
+      console.log('is authenticated', req.isAuthenticated);
+      console.log('user', req.user);
+      console.log('req.body', req.body);
+
+      const queryText = `INSERT INTO "items" ("name", "brand", 
+                            "model", "upload", "user_id")
+                            VALUES($1, $2, $3, $4, $5)`;
+
+    
+
+      
+      
+      
+  }
 });
 
 module.exports = router;
--- src/redux/sagas/add.saga.js
+++ src/redux/sagas/add.saga.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 
 function* addItem (action){
     try{
-        yield axios.post('api/item')
+        yield axios.post('api/item', action.payload)
+    }catch(err) {
+        console.log('Error in Add Item', err);
     }
-
-
 }
 
 function* addSaga(){
     yield takeLatest('ADD_ITEM', addItem);
-    yield takeLatest('ADD_ROOM', addRoom);
+    // yield takeLatest('ADD_ROOM', addRoom);
 }
+
+export default addSaga;
