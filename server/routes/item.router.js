44baf3638f342981881390c104577076f8b2b62a
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-29 15:13:31 -0500

    Succesfully posts to database

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -3,34 +3,38 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const {
     rejectUnauthenticated,
-  } = require('../modules/authentication-middleware');
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+    // GET route code here
 });
 
-/**
- * POST route template
- */
-router.post('/', rejectUnauthenticated,  (req, res) => {
-  if(req.isAuthenticated()){
-      console.log('is authenticated', req.isAuthenticated);
-      console.log('user', req.user);
-      console.log('req.body', req.body);
-
-      const queryText = `INSERT INTO "items" ("name", "brand", 
+//Post route with an endpoint to check for login
+router.post('/', rejectUnauthenticated, (req, res) => {
+    //Endpoint functionality
+    if (req.isAuthenticated()) {
+        console.log('is authenticated', req.isAuthenticated);
+        console.log('user', req.user);
+        console.log('req.body', req.body);
+        //Adding item to "items" table in the database
+        const queryText = `INSERT INTO "items" ("name", "brand", 
                             "model", "upload", "user_id")
                             VALUES($1, $2, $3, $4, $5)`;
 
-    
-
-      
-      
-      
-  }
+        pool.query(queryText, [req.body.name, req.body.brand, req.body.model, req.body.upload, req.user.id])
+            .then((result) => {
+                res.sendStatus(201);
+            }).catch(err => {
+                console.log('post error', err);
+                res.sendStatus(500);
+            });
+    } else {
+        //Send Forbidden if user isn't authenticated
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
