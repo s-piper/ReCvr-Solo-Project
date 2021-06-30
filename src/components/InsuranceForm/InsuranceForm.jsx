bb60d7c8e3ca22f7e2b5f1c2dc7e0822cacbeafe
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-30 14:44:08 -0500

    created page and form folders

---
+++ src/components/InsuranceForm/InsuranceForm.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function InsuranceForm(){
+
+    return(
+
+        <div>
+
+        </div>
+    )
+}
+
+export default InsurancePage;
---
+++ src/components/InsuranePage/InsurancePage.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function InsurancePage(){
+
+
+    return(
+
+        <div>
+
+        </div>
+    )
+}
+
+export default InsurancePage;
