936d0e787f3d82edaff00a40927e64f666d51c89
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-02 00:13:38 -0500

    setup reducer for items

---
+++ src/redux/reducers/item.reducer.js
@@ -0,0 +1,10 @@
+const itemReducer = (state=[], action) =>{
+    switch(action.type){
+        case 'SET_ITEM':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default itemReducer;
