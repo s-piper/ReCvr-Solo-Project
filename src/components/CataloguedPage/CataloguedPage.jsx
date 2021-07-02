685616ded5d31da4f72e8b0e7c929e7b176ee5eb
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-01 23:49:13 -0500

    created catalogued page setup route in app

---
+++ src/components/CataloguedPage/CataloguedPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import InsuranceForm from '../InsuranceForm/InsuranceForm';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { render } from 'react-dom';
+import { useHistory } from 'react-router-dom';
+
+function CataloguedPage(){
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default CataloguedPage;
