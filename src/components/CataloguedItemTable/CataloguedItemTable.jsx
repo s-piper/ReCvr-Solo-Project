5d068f2cfc076b6c737ab91e90ceec00f415005e
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-08 11:41:24 -0500

    added icons to items

--- src/components/CataloguedItemTable/CataloguedItemTable.jsx
+++ src/components/CataloguedItemTable/CataloguedItemTable.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
 import { useHistory } from 'react-router-dom';
 import CataloguedPdf from '../CataloguedPdf/CataloguedPdf';
 
@@ -29,7 +33,7 @@ function CataloguedItemTable() {
 
     const handleEdit = (item) => {
 
-        dispatch({type:'EDIT_ITEM', payload: item});
+        dispatch({ type: 'EDIT_ITEM', payload: item });
         history.push('/item-edit')
 
         console.log('handleEdit', item);
@@ -52,23 +56,23 @@ function CataloguedItemTable() {
                 <tbody>
                     {items.map((item, i) => {
                         return (
-                        <tr key={i}>
-                            <td>{item.name}</td>
-                            <td>{item.brand}</td>
-                            <td>{item.model}</td>
-                            <td>
-                                <img src={item.upload} width="50" height="50"/>
-                            </td>
-                            <td>{<button onClick={() => {handleEdit(item)}}>
-                                Edit
-                                </button>}
-                            </td>
-                            <td>{<button onClick={() => handleDelete (item)}>
-                                Delete
-                                </button>}
-                            </td>
-                        </tr>)
-                        })}
+                            <tr key={i}>
+                                <td>{item.name}</td>
+                                <td>{item.brand}</td>
+                                <td>{item.model}</td>
+                                <td>
+                                    <img src={item.upload} width="50" height="50" />
+                                </td>
+                                <td><IconButton onClick={() => { handleEdit(item) }}>
+                                    <EditIcon />
+                                </IconButton>
+                                </td>
+                                <td><IconButton onClick={() => handleDelete(item)}>
+                                    <DeleteIcon />
+                                </IconButton>
+                                </td>
+                            </tr>)
+                    })}
                 </tbody>
             </table>
 
